8a81ce859d445926cd00d56dc2d03ba522f80c4d
Nick Nobile <nickcnobile@gmail.com>
2021-04-18 17:14:16 -0400

    fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do
-      resources :headphones, only [:index]
+      resources :headphones, only: [:index]
     end
   end
 

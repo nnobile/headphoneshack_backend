4425633d199ff6b732a5840f91c2aa3d28004c9a
Nick Nobile <nickcnobile@gmail.com>
2021-04-18 20:41:38 -0400

    headphone controller index and create routes

--- app/controllers/api/v1/headphones_controller.rb
+++ app/controllers/api/v1/headphones_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::HeadphonesController < ApplicationController
+
+    def index
+        headphones = Headphone.all
+        render json: headphones
+    end
+
+    def create
+        headphone = Headphone.new(headphone_params)
+        if headphone.save
+            render json: headphone, status: :accepted
+        else
+            render json: {errors: headphone.error.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def headphone_params
+        params.require(:headphone).permit(:brand, :model, :description, :price, :category_id)
+    end
+
+
 end
